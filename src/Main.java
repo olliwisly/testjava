82a90e20e8d8feb103b8f2daff41439298db379e
oliver <oliver.wesely@gmx.at>
2020-10-15 10:29:13 +0200

    addtext

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String testen="hello";
+        String testen="hellofdsfdsfds";
         System.out.println(testen);
     }
 }
